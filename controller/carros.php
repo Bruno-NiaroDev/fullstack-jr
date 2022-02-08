cfd5968b4145aac196c414d3924f82bc9843c772
Bruno Araujo <bruno.araujo@sou.unifeob.edu.br>
2022-02-08 14:02:41 -0300

    Padronizando arquivo.

--- controller/carros.php
+++ controller/carros.php
@@ -3,20 +3,20 @@
  include '../model/carros.php';
 
 class CarrosController extends Carros {
-    public function getAllCars() {
-        return $_SESSION["dataCache"];
-      }
+  public function getAllCars() {
+    return $_SESSION["dataCache"];
+  }
     
-      public function getCarById($idCar) {
-        
-        return $_SESSION["dataCache"][
-          array_search(
-            $idCar, 
-            array_column(
-              $_SESSION["dataCache"], 
-              'id'
-            )
-          )];
-      }
+  public function getCarById($idCar) {
+    
+    return $_SESSION["dataCache"][
+      array_search(
+        $idCar, 
+        array_column(
+          $_SESSION["dataCache"], 
+          'id'
+        )
+      )];
+  }
 }
 ?>
