6aa342fbc6e83a0a8c2a171caf81c90fee5de8e4
Bruno Araujo <bruno.araujo@sou.unifeob.edu.br>
2022-02-07 23:37:20 -0300

    Implementado estilização de alguns componentes e adicionado um bloco informativo para deixar com uma aparencia melhor.

--- view/listarCarros.php
+++ view/listarCarros.php
@@ -8,33 +8,39 @@
 
   <link href="https://cdn.datatables.net/1.11.4/css/jquery.dataTables.min.css" rel="stylesheet">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-  
+
 </head>
 <body>
-  <table>
-    <thead>
-      <tr>
-        <th>Tipo</th>
-        <th>Categoria</th>
-        <th>Marca</th>
-        <th>Modelo</th>
-        <th>Versão</th>
-        <th>Ano Modelo</th>
-        <th>-</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td>USADO</td>
-        <td>AUTOMOVEL</td>
-        <td>Ford</td>
-        <td>EcoSport</td>
-        <td>LTZ 1.8</td>
-        <td>2008</td>
-        <td><a href="">&times;</a></td>
-      </tr>
-    </tbody>
-  </table>
+  <div class="container p-3 my-3 bg-dark text-white">
+    <h1>Teste Full Stack PHP</h1>
+    <p>Olá, sou o Bruno e aqui está a minha listagem de carros!</p>
+  </div>
+  <div class="container">
+    <table class="table table-striped table-bordered" style="width:100%">
+      <thead class="thead-light">
+        <tr>
+          <th>Tipo</th>
+          <th>Categoria</th>
+          <th>Marca</th>
+          <th>Modelo</th>
+          <th>Versão</th>
+          <th>Ano Modelo</th>
+          <th>-</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>USADO</td>
+          <td>AUTOMOVEL</td>
+          <td>Ford</td>
+          <td>EcoSport</td>
+          <td>LTZ 1.8</td>
+          <td>2008</td>
+          <td><a href="">&times;</a></td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
