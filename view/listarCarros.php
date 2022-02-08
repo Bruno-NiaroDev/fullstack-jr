6d0110b16427ba966f0cd784c1f9d67e87db36ca
Bruno Araujo <bruno.araujo@sou.unifeob.edu.br>
2022-02-07 23:32:16 -0300

    Criado estrutura basica da tabela.

--- view/listarCarros.php
+++ view/listarCarros.php
@@ -1,12 +1,35 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt_bt">
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Listagem</title>
+  <title>Teste Full Stack PHP - Bruno Araujo</title>
 </head>
 <body>
-  <h1>Aqui ficara minha listagem</h1>
+  <table>
+    <thead>
+      <tr>
+        <th>Tipo</th>
+        <th>Categoria</th>
+        <th>Marca</th>
+        <th>Modelo</th>
+        <th>Versão</th>
+        <th>Ano Modelo</th>
+        <th>-</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>USADO</td>
+        <td>AUTOMOVEL</td>
+        <td>Ford</td>
+        <td>EcoSport</td>
+        <td>LTZ 1.8</td>
+        <td>2008</td>
+        <td><a href="">&times;</a></td>
+      </tr>
+    </tbody>
+  </table>
 </body>
 </html>
