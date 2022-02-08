c16d033ed16a202ee89b403272fac703b87cf996
Bruno Araujo <bruno.araujo@sou.unifeob.edu.br>
2022-02-08 13:34:29 -0300

    Criado estrutura base para exibição do modal de detalhe do carro.

---
+++ view/detalharCarro.php
@@ -0,0 +1,24 @@
+<div class="container">
+  <div class="row">
+    <div class="col-sm-7">
+      <h2>Renault <span class="text-danger">Kwid</span></h2>
+      <p class="text-secondary">Kwid 1.0 Zen</p>
+    </div>
+    <div class="col-sm-5">
+      <h4>R$ <span class="text-danger">45.990,00</span></h4>
+    </div>
+    <div class="col-sm-4">Tipo: <b>USADOS</b></div>
+    <div class="col-sm-4">Categoria: <b>AUTOMOVEL</b></div>
+    <div class="col-sm-4">Segmento: <b>Hatch</b></div>
+    <div class="col-sm-4">Ano De Fabricação: <b>2019</b></div>
+    <div class="col-sm-4">Ano Do Modelo: <b>2020</b></div>
+    <div class="col-sm-4">Portas: <b>4</b></div>
+    <div class="col-sm-12">
+      <h5>Descrição</h5>
+      <p>NISSAN SENTRA 2.0, carro super novo, COMPLETO, ar condicionado, vidros, travas e retrovisores elÃ©tricos, airbags, impecÃ¡vel! Venha pra Giva Car, o melhor negocio estÃ¡ aqui! Aceitamos seu usado como parte de pagamento, financiamos e parcelamos a entrada no cartÃ£o de credito. Venha conhecer nossa loja! Avenida BarÃ£o de Monte Alegre, 129, Bonfim, em campinas. Reservamos o direito o direito de corrigir erros de digitaÃ§Ã£o. Telefone e WhatsApp (19)33956875.</p>
+    </div>
+    <div class="col-sm-12">
+      <p class="text-muted">Anúncio Criado em: 23/07/2021 18:07</p>
+    </div>
+  </div>
+</div>
