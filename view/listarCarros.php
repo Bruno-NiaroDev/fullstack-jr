8a388f697627aad0bab8ed48ced669271adf3b37
Bruno Araujo <bruno.araujo@sou.unifeob.edu.br>
2022-02-08 13:36:22 -0300

    Adicionado botão que fará uma requisição ajax para alimentar o conteudo do modal e exibi-lo para o cliente.

--- view/listarCarros.php
+++ view/listarCarros.php
@@ -8,8 +8,9 @@
   <title>Teste Full Stack PHP - Bruno Araujo</title>
 
   <link href="https://cdn.datatables.net/1.11.4/css/jquery.dataTables.min.css" rel="stylesheet">
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <link href="../assets/css/style.css" rel="stylesheet">
 
 </head>
 <body>
@@ -20,7 +21,6 @@
   </div>
   <br>
   <div class="container" >
-
     <table id="myTable" class="table-responsive table-striped table-bordered" style="width:100%">
       <thead class="thead-light">
         <tr>
@@ -45,19 +45,37 @@
           <td><?php echo $car['modelo']; ?></td>
           <td><?php echo $car['versao']; ?></td>
           <td><?php echo $car['ano_modelo']; ?></td>
-          <td><a href="" style="color: grey"><i class="fas fa-search-plus"></i></a></td>
+          <td>
+            <button id="<?php echo $car['id']; ?>" type="button" class="carInfo btn btn-link" >
+              <i class="fas fa-search-plus"></i>
+            </button>
+          </td>
         </tr>
       <?php
         }
       ?>
       </tbody>
-    </table>
+    </table>    
+  </div>
+  <div class="modal" id="myModal">
+    <div class="modal-dialog modal-lg">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h4 class="modal-title">Detalhes: </h4>
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+        </div>
+        <div class="modal-body">
+          
+        </div>
+      </div>
+    </div>
   </div>
 
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://code.jquery.com/jquery-3.3.1.min.js" ></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
   <script src="https://cdn.datatables.net/1.11.4/js/jquery.dataTables.min.js"></script>
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
   <script>
     $(document).ready(function(){
       $('#myTable').DataTable({
@@ -75,6 +93,19 @@
             "last": "Último"
           }
         }
+      });
+
+      $('.carInfo').click(function(){
+        var carId = $(this).attr('id');
+        
+        $.ajax({
+          url: 'detalharCarro.php?carId='+carId,
+          type: 'get',
+          success: function(response){ 
+            $('.modal-body').html(response);
+            $('#myModal').modal('show')
+          }
+        });
       });
     });
   </script>
