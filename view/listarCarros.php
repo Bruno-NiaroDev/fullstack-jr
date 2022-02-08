8482d6b21942de93c976379eba87e37d3da1a584
Bruno Araujo <bruno.araujo@sou.unifeob.edu.br>
2022-02-07 23:52:04 -0300

    Adicionado tradução das labels utilizadas pelo DataTable e também incluido a lib fontawesome para icones.

--- view/listarCarros.php
+++ view/listarCarros.php
@@ -8,12 +8,13 @@
 
   <link href="https://cdn.datatables.net/1.11.4/css/jquery.dataTables.min.css" rel="stylesheet">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
 </head>
 <body>
   <div class="container p-3 my-3 bg-dark text-white">
-    <h1>Teste Full Stack PHP</h1>
-    <p>Olá, sou o Bruno e aqui está a minha listagem de carros!</p>
+    <h1><i class="fas fa-vial"></i> Teste Full Stack PHP</h1>
+    <p>Olá, sou o Bruno e aqui está a minha listagem de carros! <i class="far fa-smile-wink"></i></p>
   </div>
   <div class="container">
     <table id="myTable" class="table table-striped table-bordered" style="width:100%">
@@ -36,7 +37,7 @@
           <td>EcoSport</td>
           <td>LTZ 1.8</td>
           <td>2008</td>
-          <td><a href="">&times;</a></td>
+          <td><a href="" style="color: grey"><i class="fas fa-search-plus"></i></a></td>
         </tr>
       </tbody>
     </table>
@@ -45,10 +46,25 @@
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
   <script src="https://cdn.datatables.net/1.11.4/js/jquery.dataTables.min.js"></script>
-
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <script>
     $(document).ready(function(){
-      $('#myTable').DataTable();
+      $('#myTable').DataTable({
+        "language": {
+          "lengthMenu": "Mostrando _MENU_ registros por página",
+          "zeroRecords": "Nada encontrado",
+          "info": "Mostrando página _PAGE_ de _PAGES_",
+          "infoEmpty": "Nenhum registro disponível",
+          "infoFiltered": "(filtrado de _MAX_ registros no total)",
+          "search": "Pesquisar",
+          "paginate": {
+            "next": "Próximo",
+            "previous": "Anterior",
+            "first": "Primeiro",
+            "last": "Último"
+          }
+        }
+      });
     });
   </script>
 
